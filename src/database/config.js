996cdd5298b36b9cebb8fcf863d5500be0d7ebd9
Lautaro Valentini <lautiivalentini@gmail.com>
2021-11-10 09:49:03 -0300

    ToDo: set up correct production url

--- src/database/config.js
+++ src/database/config.js
@@ -2,6 +2,7 @@ const config = {
     DB_NAME: "miciudad",
     DB_URL: {
         dev: "mongodb://localhost:27017",
+        // TODO: set up correct production url
         prod: "mongodb+srv://user:password@cluster0.7cpxz.mongodb.net/posts?retryWrites=true&w=majority"
     },
 };
