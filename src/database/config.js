0fe89d6493f4b6e0c7d4fb2531480ddf779130d6
Lautaro Valentini <lautiivalentini@gmail.com>
2021-11-10 00:47:52 -0300

    Add MongoDB connection

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,9 @@
-let config = {
-    dbUrl:
-    // Connect to Cluster0    
-      "mongodb+srv://user:password@cluster0.7cpxz.mongodb.net/posts?retryWrites=true&w=majority",
-  };
-  
-  module.exports = config;
+const config = {
+    DB_NAME: "miciudad",
+    DB_URL: {
+        dev: "mongodb://localhost:27017",
+        prod: "mongodb+srv://user:password@cluster0.7cpxz.mongodb.net/posts?retryWrites=true&w=majority"
+    },
+};
+
+module.exports = config;
