0c8c8edee2f2ad1bf87b2520e7c240b28eadade6
brahianpdev <zk.spywn@gmail.com>
2021-11-13 13:36:42 -0300

    feat: get path /extravios

---
+++ src/routes/lostItems.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = Router();
+
+const controller = require('../controllers/lostItems');
+
+const path = 'extravios';
+
+router.get(
+    `/${path}`,
+    controller.getData
+)
+
+module.exports = router;
