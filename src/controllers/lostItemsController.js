4d0516e6923c1d1b38b1122ec1e73838a90a78b1
Brahianpdev <zkspywn@gmail.com>
2021-12-05 11:40:23 -0300

    feat: Convert arrow functions controller to Class Controller

--- src/controllers/lostItemsController.js
+++ src/controllers/lostItemsController.js
@@ -1,50 +1,54 @@
-const Lost_Items = require("../models/LostItem");
+/* eslint-disable new-cap */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-len */
+const lostItems = require('./lostItems');
 
-exports.getLostItems = async (req, res) => {
-  try {
-    const lostItems = await Lost_Items.find();
-    return res.status(200).json(lostItems);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+class lostItemsController {
+  async getLostItems(req, res) {
+    try {
+      const myLostItems = await lostItems.find();
+      return res.status(200).json(myLostItems);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
   }
-};
 
-exports.postLostItem = async (req, res) => {
-  try {
-    const newLostItem = new Lost_Items(req.body);
-    const lostItem = await newLostItem.save();
-    return res.status(201).json(lostItem);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+  async postLostItems(req, res) {
+    try {
+      const newLostItem = new lostItems(req.body);
+      const lostItem = await newLostItem.save();
+      return res.status(201).json(lostItem);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
   }
-};
 
-exports.updateLostItem = async (req, res) => {
-  try {
-    const updateLostItem = await Lost_Items.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true, //valida nuevamente el esquema para evitar ingresar datos no validados
-      }
-    );
-    // console.log(req.params.id, req.body )
-    if (!updateLostItem)
-      return res.status(404).json({ message: "Lost Item not found" });
-    return res.status(200).json(updateLostItem);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+  async updateLostItems(req, res) {
+    try {
+      const updateLostItem = await lostItems.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+          runValidators: true, // valida nuevamente el esquema para evitar ingresar datos no validados
+        },
+      );
+      if (!updateLostItem) return res.status(404).json({ message: 'Lost Item not found' });
+      return res.status(200).json(updateLostItem);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
   }
-};
 
-exports.deleteLostItem = async (req, res) => {
-  try {
-    const deletedLostItem = await Lost_Items.findByIdAndDelete(req.params.id);
-    if (!deletedLostItem)
-      return res.status(404).json({ message: "Lost Item not found" });
-    return res.status(204).json();
-  } catch (error) {
-    return res.status(400).json({ msg: error.message });
+  async deleteLostItems(req, res) {
+    try {
+      const deletedLostItem = await lostItems.findByIdAndDelete(req.params.id);
+      if (!deletedLostItem) return res.status(404).json({ message: 'Lost Item not found' });
+      return res.status(204).json();
+    } catch (error) {
+      return res.status(400).json({ msg: error.message });
+    }
   }
-};
+}
+
+module.exports = new lostItemsController();
