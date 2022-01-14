aec79380c23cdc46fa0599f53d8ddf670d64bb92
martindevaluado <pmartinacosta@gmail.com>
2022-01-14 20:56:58 -0300

    fix: change default express port to 8080

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 require('dotenv').config()
